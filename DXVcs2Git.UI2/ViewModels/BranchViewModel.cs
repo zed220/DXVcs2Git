a0c88de5bc453f3fe22eaf15093ee03db3511677
zinovyev.petr <petr.zinovyev@devexpress.com>
2017-05-10 15:05:00 +0300

    continue

---
+++ DXVcs2Git.UI2/ViewModels/BranchViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.Mvvm;
+using DXVcs2Git.Git;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXVcs2Git.UI2 {
+    public class BranchViewModel : ViewModelBase {
+        public string Name { get; }
+
+        public BranchViewModel(GitLabWrapper gitLabWrapper, RepositoryViewModel repository, string branch) {
+            Name = branch;
+        }
+    }
+}
--- DXVcs2Git.UI2/ViewModels/RepositoryViewModel.cs
+++ DXVcs2Git.UI2/ViewModels/RepositoryViewModel.cs
@@ -1,21 +1,47 @@
-﻿using DevExpress.Mvvm;
-using DXVcs2Git.Core.Configuration;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DevExpress.Mvvm;
+using DevExpress.Mvvm.Native;
+using DXVcs2Git.Core;
+using DXVcs2Git.Core.Configuration;
+using DXVcs2Git.Core.Git;
+using DXVcs2Git.Git;
+using NGitLab.Models;
+using System.Collections.ObjectModel;
+using LibGit2Sharp;
 
 namespace DXVcs2Git.UI2 {
     public class RepositoryViewModel : ViewModelBase {
+        public string Name { get; }
+        public Project Origin { get; }
+        GitLabWrapper GitLabWrapper { get; }
+        GitReaderWrapper GitReader { get; }
+        public TrackRepository TrackRepository { get; }
+        public ObservableCollection<BranchViewModel> Branches {
+            get { return GetProperty(() => Branches); }
+            set { SetProperty(() => Branches, value); }
+        }
+
         public RepositoryViewModel(string name, TrackRepository trackRepository, RepositoriesViewModel repositories) {
             Name = name;
+            TrackRepository = trackRepository;
+            GitLabWrapper = new GitLabWrapper(TrackRepository.Server, TrackRepository.Token);
+            GitReader = new GitReaderWrapper(trackRepository.LocalPath);
+            Origin = GitLabWrapper.FindProject(GitReader.GetOriginRepoPath());
+            LoadBranches();
         }
 
-        public string Name {
-            get { return GetProperty(() => Name); }
-            set { SetProperty(() => Name, value); }
-        }
+        void LoadBranches() {
+            if(Origin == null) {
+                //Log.Error("Can`t find project");
+                return;
+            }
 
+            var branches = this.GitLabWrapper.GetBranches(Origin).ToList();
+            var localBranches = GitReader.GetLocalBranches();
+            Branches = new ObservableCollection<BranchViewModel>(branches.Where(x => !x.Protected && localBranches.Any(local => local.FriendlyName == x.Name))
+                .Select(x => new BranchViewModel(GitLabWrapper, this, x.Name)));
+
+        }
     }
 }
